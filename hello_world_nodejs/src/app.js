7edec93d26510d78c68dacf7fa64e162e9835059
Soumya Pal <pal.soumya1996@gmail.com>
2021-07-28 12:41:15 -0400

    update

---
+++ hello_world_nodejs/src/app.js
@@ -0,0 +1,26 @@
+const fsPromises = require('fs').promises;
+const figlet = require('figlet');
+
+(async () => {
+  try {
+    const iexecOut = process.env.IEXEC_OUT;
+    // Do whatever you want (let's write hello world here)
+    const message = process.argv.length > 2 ? process.argv[2] : 'World';
+
+    const text = figlet.textSync(`Hello, ${message}!`); // Let's add some art for e.g.
+    console.log(text);
+    // Append some results in /iexec_out/
+    await fsPromises.writeFile(`${iexecOut}/result.txt`, text);
+    // Declare everything is computed
+    const computedJsonObj = {
+      'deterministic-output-path': `${iexecOut}/result.txt`,
+    };
+    await fsPromises.writeFile(
+      `${iexecOut}/computed.json`,
+      JSON.stringify(computedJsonObj),
+    );
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
