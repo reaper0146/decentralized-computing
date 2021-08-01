ce2cfffecf58f731824ad8f87799cfce88e82ec0
Soumya Pal <pal.soumya1996@gmail.com>
2021-08-01 14:17:42 -0400

    update

--- filecoin/src/index.js
+++ filecoin/src/index.js
@@ -9,7 +9,21 @@ STYLES_PAGES = css "
 ";
 
 export default class Testpage extends React.Component {
+    componentDidMount() {
+        const response = await fetch('https://slate.host/api/v2/update-file', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'SLA74d74d8e-1eb5-42c6-968b-a121644f5064TE', // API key
+        },
+        body: JSON.stringify({ data: file })
+    });
+        const json = await response.json();
+        }
     _handleUpload = (e) => {
+        e.persist();
+
+        console.log(e)
 
     }
     render() {
