15256f68390b7d5594075efcf3ea05448f66f42a
Soumya Pal <pal.soumya1996@gmail.com>
2021-08-16 11:34:37 -0400

    update

--- golem/deml/requestor.py
+++ golem/deml/requestor.py
@@ -12,9 +12,10 @@ from datetime import timedelta
 from pathlib import Path
 from typing import AsyncIterable, Iterator
 
-from yapapi import Task, WorkContext
+from yapapi import Golem, Task, WorkContext
 from yapapi.log import enable_default_logger
 from yapapi.payload import vm
+import json
 
 import worker
 
@@ -37,6 +38,15 @@ def data(words_file: Path, chunk_size: int = 100_000) -> Iterator[Task]:
     Return an iterator of `Task` objects.
     """
     # TODO
+    with words_file.open() as f:
+        chunk = []
+        for line in f:
+            chunk.append(line.strip())
+            if len(chunk) == chunk_size:
+                yield Task(data=chunk)
+                chunk = []
+        if chunk:
+            yield Task(data=chunk)
 
 async def steps(context: WorkContext, tasks: AsyncIterable[Task]):
     """Prepare a sequence of steps which need to happen for a task to be computed.
@@ -47,16 +57,53 @@ async def steps(context: WorkContext, tasks: AsyncIterable[Task]):
     The signature of this function cannot change, as it's used internally by `Executor`.
     """
     # TODO
+    context.send_file(str(args.hash), str(worker.HASH_PATH))
+
+    async for task in tasks:
+        context.send_json(str(worker.WORDS_PATH), task.data)
+
+        context.run(str(ENTRYPOINT_PATH))
+
+        # Create a temporary file to avoid overwriting incoming results
+        output_file = 'out.json' #Path(worker.RESULT_PATH) # / str(uuid4()) #NamedTemporaryFile()
+        context.download_file(str(worker.RESULT_PATH), output_file)
+
+        # Pass the prepared sequence of steps to Executor
+        yield context.commit()
+
+        # Mark task as accepted and set its result
+        task.accept_result(result=json.load(output_file))
+        output_file.close()
 
 async def main():
     # Set of parameters for the VM run by each of the providers
     package = await vm.repo(
-        image_hash="1e53d1f82b4c49b111196fcb4653fce31face122a174d9c60d06cf9a",
+        image_hash="d7098f41e0a4dfbc43c8b58b672dcf69a5ded996d5466c1b6c46c428",
         min_mem_gib=1.0,
         min_storage_gib=2.0,
     )
 
     # TODO Run Executor using data and steps functions
+    async with Golem(budget=1, subnet_tag=args.subnet) as golem:
+
+        result = ""
+
+        async for task in golem.execute_tasks(
+            steps,
+            data(args.words),
+            payload=package,
+            timeout=TASK_TIMEOUT
+        ):
+            # Every task object we receive here represents a computed task
+            if task.result:
+                result = task.result
+                # Exit early once a matching word is found
+                break
+
+        if result:
+            print(f"Found matching word: {result}")
+        else:
+            print("No matching words found.")
 
 if __name__ == "__main__":
     args = arg_parser.parse_args()
